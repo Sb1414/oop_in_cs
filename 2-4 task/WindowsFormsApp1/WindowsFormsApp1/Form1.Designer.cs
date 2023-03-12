5af981c0b586344dd493dd40bfe51748a66234fb
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-03-12 23:47:00 +0300

    formApp

---
+++ 2-4 task/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.back = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxPay3 = new System.Windows.Forms.TextBox();
+            this.textBoxPay2 = new System.Windows.Forms.TextBox();
+            this.Оценки = new System.Windows.Forms.Label();
+            this.textBoxPay1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCredit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panelUp = new System.Windows.Forms.Panel();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textBoxPayment = new System.Windows.Forms.TextBox();
+            this.buttonAddPay = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.back.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panelUp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // back
+            // 
+            this.back.BackColor = System.Drawing.Color.Bisque;
+            this.back.Controls.Add(this.buttonAddPay);
+            this.back.Controls.Add(this.textBoxPayment);
+            this.back.Controls.Add(this.button2);
+            this.back.Controls.Add(this.button1);
+            this.back.Controls.Add(this.buttonAdd);
+            this.back.Controls.Add(this.textBoxPay3);
+            this.back.Controls.Add(this.textBoxPay2);
+            this.back.Controls.Add(this.Оценки);
+            this.back.Controls.Add(this.textBoxPay1);
+            this.back.Controls.Add(this.label2);
+            this.back.Controls.Add(this.textBoxCredit);
+            this.back.Controls.Add(this.label1);
+            this.back.Controls.Add(this.textBoxName);
+            this.back.Controls.Add(this.dataGridView1);
+            this.back.Controls.Add(this.panelUp);
+            this.back.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.back.Location = new System.Drawing.Point(0, 0);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(1194, 755);
+            this.back.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.NavajoWhite;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Wheat;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1024, 642);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 41);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Wheat;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 585);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 64);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Добавить пустую строку";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.NavajoWhite;
+            this.buttonAdd.FlatAppearance.BorderColor = System.Drawing.Color.Wheat;
+            this.buttonAdd.FlatAppearance.BorderSize = 0;
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAdd.Font = new System.Drawing.Font("Arial Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(1024, 585);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(146, 41);
+            this.buttonAdd.TabIndex = 13;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxPay3
+            // 
+            this.textBoxPay3.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPay3.Location = new System.Drawing.Point(882, 620);
+            this.textBoxPay3.Name = "textBoxPay3";
+            this.textBoxPay3.Size = new System.Drawing.Size(111, 29);
+            this.textBoxPay3.TabIndex = 23;
+            // 
+            // textBoxPay2
+            // 
+            this.textBoxPay2.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPay2.Location = new System.Drawing.Point(765, 620);
+            this.textBoxPay2.Name = "textBoxPay2";
+            this.textBoxPay2.Size = new System.Drawing.Size(111, 29);
+            this.textBoxPay2.TabIndex = 22;
+            // 
+            // Оценки
+            // 
+            this.Оценки.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Оценки.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Оценки.Location = new System.Drawing.Point(657, 585);
+            this.Оценки.Name = "Оценки";
+            this.Оценки.Size = new System.Drawing.Size(102, 23);
+            this.Оценки.TabIndex = 22;
+            this.Оценки.Text = "Выплаты";
+            this.Оценки.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxPay1
+            // 
+            this.textBoxPay1.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPay1.Location = new System.Drawing.Point(648, 620);
+            this.textBoxPay1.Name = "textBoxPay1";
+            this.textBoxPay1.Size = new System.Drawing.Size(111, 29);
+            this.textBoxPay1.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(193, 623);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 23);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Кредит";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxCredit
+            // 
+            this.textBoxCredit.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxCredit.Location = new System.Drawing.Point(329, 620);
+            this.textBoxCredit.Name = "textBoxCredit";
+            this.textBoxCredit.Size = new System.Drawing.Size(265, 29);
+            this.textBoxCredit.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(189, 588);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 23);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Фамилия";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(329, 585);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(265, 29);
+            this.textBoxName.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Bisque;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1170, 532);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // panelUp
+            // 
+            this.panelUp.Controls.Add(this.buttonClose);
+            this.panelUp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelUp.Location = new System.Drawing.Point(0, 0);
+            this.panelUp.Name = "panelUp";
+            this.panelUp.Size = new System.Drawing.Size(1194, 30);
+            this.panelUp.TabIndex = 0;
+            this.panelUp.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelUp_MouseDown);
+            this.panelUp.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelUp_MouseMove);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.NavajoWhite;
+            this.buttonClose.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonClose.FlatAppearance.BorderColor = System.Drawing.Color.Wheat;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Arial Black", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClose.Location = new System.Drawing.Point(0, 0);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(30, 30);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textBoxPayment
+            // 
+            this.textBoxPayment.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPayment.Location = new System.Drawing.Point(329, 676);
+            this.textBoxPayment.Name = "textBoxPayment";
+            this.textBoxPayment.Size = new System.Drawing.Size(147, 29);
+            this.textBoxPayment.TabIndex = 29;
+            // 
+            // buttonAddPay
+            // 
+            this.buttonAddPay.BackColor = System.Drawing.Color.NavajoWhite;
+            this.buttonAddPay.FlatAppearance.BorderColor = System.Drawing.Color.Wheat;
+            this.buttonAddPay.FlatAppearance.BorderSize = 0;
+            this.buttonAddPay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAddPay.Font = new System.Drawing.Font("Arial Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAddPay.Location = new System.Drawing.Point(495, 676);
+            this.buttonAddPay.Name = "buttonAddPay";
+            this.buttonAddPay.Size = new System.Drawing.Size(227, 29);
+            this.buttonAddPay.TabIndex = 30;
+            this.buttonAddPay.Text = "Добавить выплату";
+            this.buttonAddPay.UseVisualStyleBackColor = false;
+            this.buttonAddPay.Click += new System.EventHandler(this.buttonAddPay_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Фамилия";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Кредит";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 90;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Выплаты";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 250;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Сумма выплат";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 90;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Количество выплат";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 50;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Осталось до погашения";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1194, 755);
+            this.Controls.Add(this.back);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.back.ResumeLayout(false);
+            this.back.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panelUp.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel back;
+        private System.Windows.Forms.Panel panelUp;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxPay3;
+        private System.Windows.Forms.TextBox textBoxPay2;
+        private System.Windows.Forms.Label Оценки;
+        private System.Windows.Forms.TextBox textBoxPay1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCredit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonAddPay;
+        private System.Windows.Forms.TextBox textBoxPayment;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
