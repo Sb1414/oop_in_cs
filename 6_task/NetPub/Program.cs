81daa0e27b68f6fcc1240166f1fd291c859d64e8
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-04-17 14:11:39 +0300

    Publication

---
+++ 6_task/NetPub/Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetPub;
+using System;
+
+bool fl = true;
+var manager = new PublicationBase();
+while (fl)
+{
+    menu();
+    int num;
+    num = Console.ReadKey().KeyChar - '0';
+    switch (num)
+    {
+        case 1:
+            draw();
+            AddArticle();
+            break;
+        case 2:
+            draw();
+            AddNews();
+            break;
+        case 3:
+            draw();
+            Console.WriteLine("\nВывод статей:");
+            manager.PrintPublicationsByType<Article>();
+            draw();
+            break;
+        case 4:
+            draw();
+            Console.WriteLine("\nВывод новостей:");
+            manager.PrintPublicationsByType<News>();
+            draw();
+            break;
+        case 5:
+            draw();
+            Console.WriteLine("\nВывод всех публикаций:");
+            manager.PrintAllPublications();
+            draw();
+            break;
+        case 6:
+            draw();
+            RemoveArticle();
+            draw();
+            break;
+        case 7:
+            draw();
+            RemoveNews();
+            draw();
+            break;
+        default:
+            fl = false;
+            break;
+    }
+}
+
+void AddNews()
+{
+
+    Console.WriteLine("\nСколько новостей внести?");
+    int n = Console.ReadKey().KeyChar - '0';
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"\nВведите заголовок новости {i}: ");
+        var title = Console.ReadLine();
+        Console.Write($"\nВведите источник новости {i}: ");
+        var source = Console.ReadLine();
+        var publication = new News(title, source);
+        manager.AddPublication(publication);
+    }
+}
+
+
+void AddArticle()
+{
+    Console.WriteLine("\nСколько статей внести?");
+    int n = Console.ReadKey().KeyChar - '0';
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"\nВведите заголовок статьи {i}: ");
+        var title = Console.ReadLine();
+        Console.Write($"\nВведите автора статьи {i}: ");
+        var author = Console.ReadLine();
+        var publication = new Article(title, author);
+        manager.AddPublication(publication);
+    }
+}
+
+
+void RemoveArticle()
+{
+    Console.Write("\nВведите заголовок статьи, которую хотите удалить: ");
+    var title = Console.ReadLine();
+    manager.RemoveArticle(title);
+}
+
+void RemoveNews()
+{
+    Console.Write("\nВведите заголовок новости, которую хотите удалить: ");
+    var title = Console.ReadLine();
+    manager.RemoveNews(title);
+}
+
+void menu()
+{
+    Console.Write("1. Внести данные о статье\n");
+    Console.Write("2. Внести данные о новости\n");
+    Console.Write("3. Вывести все статьи\n");
+    Console.Write("4. Вывести все новости\n");
+    Console.Write("5. Вывести все данные\n");
+    Console.Write("6. Удалить статью\n");
+    Console.Write("7. Удалить новость\n");
+}
+
+
+void draw()
+{
+    Console.WriteLine("\n========================================================");
+}
---
+++ 6_task/NetPub/PublicationBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetPub
+{
+    class PublicationBase
+    {
+        private readonly List<Publication> _publications = new List<Publication>();
+
+        public void AddPublication(Publication publication)
+        {
+            _publications.Add(publication);
+        }
+
+        public void PrintAllPublications()
+        {
+            foreach (var publication in _publications)
+            {
+                publication.PrintInfo();
+            }
+        }
+
+        public void PrintPublicationsByType<T>() where T : Publication
+        {
+            foreach (var publication in _publications)
+            {
+                if (publication is T)
+                {
+                    publication.PrintInfo();
+                }
+            }
+        }
+
+        public void RemoveArticle(string title)
+        {
+            var articles = _publications.OfType<Article>().Where(a => a.Title == title).ToList();
+
+            if (articles.Any())
+            {
+                foreach (var article in articles)
+                {
+                    _publications.Remove(article);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Статья с таким заголовком не найдена.");
+            }
+        }
+
+
+        public void RemoveNews(string title)
+        {
+            foreach (var publication in _publications)
+            {
+                if (publication.GetType() == typeof(News))
+                {
+                    var news = (News)publication;
+                    if (news.Title == title)
+                    {
+                        _publications.Remove(publication);
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+
+}
