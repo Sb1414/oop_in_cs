f7e743a09c37cbd47b585957dd4e2c3377ad4b16
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-05-15 00:33:38 +0300

    task 9

---
+++ 9_task/StudentEmployee/Program.cs
@@ -0,0 +1,71 @@
+﻿using StudentEmployee;
+
+IPersonContainer container = new PersonArrayContainer();
+
+container.Add(new Student("Первый") { Tuition = 50000 });
+container.Add(new Employee("Второй") { Salary = 20000 });
+container.Add(new Student_Employee("Третий") { Tuition = 50000, Salary = 25000 });
+
+foreach (var person in container.GetAll())
+{
+    Console.Write(person.LastName + " ");
+    if (person is Student)
+        Console.Write("Tuition: " + ((Student)person).Tuition + " ");
+    if (person is Employee)
+        Console.Write("Salary: " + ((Employee)person).Salary + " ");
+    if (person is Student_Employee)
+        Console.Write("Total Payment: " + ((Student_Employee)person).TotalPayment + " ");
+    Console.WriteLine();
+}
+
+/*
+    var student1 = new Student("Ivanov");
+    student1.Tuition = 1000;
+
+    var student2 = new Student("Petrov");
+    student2.Tuition = 1500;
+
+    var employee1 = new Employee("Sidorov");
+    employee1.Salary = 2000;
+
+    var employee2 = new Employee("Kozlov");
+    employee2.Salary = 2500;
+
+    var studentEmployee1 = new Student_Employee("Vasiliev");
+    studentEmployee1.Tuition = 800;
+    studentEmployee1.Salary = 1800;
+
+    var studentEmployee2 = new Student_Employee("Goncharov");
+    studentEmployee2.Tuition = 1200;
+    studentEmployee2.Salary = 2200;
+
+    // Создаем контейнер и добавляем объекты
+    var container = new PersonArrayContainer();
+    container.Add(student1);
+    container.Add(student2);
+    container.Add(employee1);
+    container.Add(employee2);
+    container.Add(studentEmployee1);
+    container.Add(studentEmployee2);
+
+    // Выводим информацию о всех объектах в контейнере
+    var allPersons = container.GetAll();
+    foreach (var person in allPersons)
+    {
+        Console.WriteLine($"Last Name: {person.LastName}");
+        if (person is Student student)
+        {
+            Console.WriteLine($"Tuition: {student.Tuition}");
+        }
+        else if (person is Employee employee)
+        {
+            Console.WriteLine($"Salary: {employee.Salary}");
+        }
+        else if (person is Student_Employee studentEmployee)
+        {
+            Console.WriteLine($"Tuition: {studentEmployee.Tuition}, Salary: {studentEmployee.Salary}, Total Payment: {studentEmployee.TotalPayment}");
+        }
+        Console.WriteLine();
+    }
+
+*/
---
+++ 9_task/StudentEmployee/StudentEmployee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEmployee
+{
+    public class Person
+    {
+        public Person(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public string LastName { get; set; }
+    }
+
+    public class Student : Person
+    {
+        private double tuition;
+
+        public double Tuition
+        {
+            get { return tuition; }
+            set { tuition = value; }
+        }
+
+        public Student(string lastName) : base(lastName) {}
+    }
+
+    public class Employee : Person
+    {
+
+        private double salary;
+
+        public double Salary
+    {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public Employee(string lastName) : base(lastName) {}
+    }
+
+    public interface IStudent
+    {
+        double Tuition { get; set; }
+    }
+
+    public interface IEmployee
+    {
+        double Salary { get; set; }
+    }
+
+    public class Student_Employee : Person, IStudent, IEmployee
+    {
+        public double Tuition { get; set; }
+        public double Salary { get; set; }
+
+        public double TotalPayment { get { return Tuition + Salary; } }
+
+        public Student_Employee(string lastName) : base(lastName) {}
+    }
+
+    // интерфейсный класс для контейнера объектов
+    interface IPersonContainer
+    {
+        void Add(Person person);
+        Person Get(int index);
+        List<Person> GetAll();
+    }
+
+    // реализация контейнера объектов на основе массива полиморфных ссылок
+    class PersonArrayContainer : IPersonContainer
+    {
+        private Person[] persons = new Person[0];
+
+        public void Add(Person person)
+        {
+            Array.Resize(ref persons, persons.Length + 1);
+            persons[persons.Length - 1] = person;
+        }
+
+        public Person Get(int index)
+        {
+            if (index >= 0 && index < persons.Length)
+                return persons[index];
+            else
+                return null;
+        }
+
+        public List<Person> GetAll()
+        {
+            return new List<Person>(persons);
+        }
+    }
+}
