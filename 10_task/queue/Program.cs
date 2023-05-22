3097effd9c74fc6cb250b585af44f3e8f341d130
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-05-22 13:57:21 +0300

    10 task

---
+++ 10_task/queue/Program.cs
@@ -0,0 +1,82 @@
+﻿using queue;
+using System;
+
+bool fl2 = true;
+Console.WriteLine("\nНа сколько элементов создать очередь?");
+int cnt = Console.ReadKey().KeyChar - '0';
+MyQueue<int> intQueue = new MyQueue<int>(cnt);
+MyQueue<string> stringQueue = new MyQueue<string>(cnt);
+Console.WriteLine("\nВыберите тип очереди:");
+MenuType();
+int type;
+type = Console.ReadKey().KeyChar - '0';
+Console.WriteLine("type is " + type);
+while (fl2)
+{
+    menu();
+    int num;
+    num = Console.ReadKey().KeyChar - '0';
+    Console.WriteLine();
+    switch (num)
+    {
+        case 1:
+            draw();
+            if (type == 1)
+            {
+                string tmp1 = Console.ReadLine();
+                Console.WriteLine();
+                stringQueue.Enqueue(tmp1);
+            } else if (type == 2)
+            {
+                int tmp2 = Console.ReadKey().KeyChar - '0';
+                Console.WriteLine();
+                intQueue.Enqueue(tmp2);
+            }
+            draw();
+            break;
+        case 2:
+            draw();
+            if (type == 1)
+            {
+                Console.WriteLine(stringQueue.Dequeue());
+            } else if (type == 2)
+            {
+                Console.WriteLine(intQueue.Dequeue());
+            }
+            draw();
+            break;
+        case 3:
+            draw();
+            if (type == 1)
+            {
+                stringQueue.Print();
+            } else if (type == 2)
+            {
+                intQueue.Print();
+            }
+            draw();
+            break;
+        default:
+            fl2 = false;
+            break;
+    }
+}
+
+void menu()
+{
+    Console.Write("\n Выберите действие! \n");
+    Console.Write("\n1. Внести данные\n");
+    Console.Write("2. Удалить данные\n");
+    Console.Write("3. Вывести данные\n");
+}
+
+void MenuType()
+{
+    Console.Write("\n1. Строковый\n");
+    Console.Write("2. Числовой\n");
+}
+
+void draw()
+{
+    Console.WriteLine("\n========================================================");
+}
---
+++ 10_task/queue/Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    class MyQueue<T>
+    {
+        private T[] data;
+        private int head; // индекс первого элемента
+        private int tail; // индекс последнего элемента
+        private int size; // размер очереди
+        private int count; // текущее количество элементов
+
+        public MyQueue(int size)
+        {
+            data = new T[size];
+            head = 0;
+            tail = -1;
+            this.size = size;
+            count = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (count == size)
+            {
+                // throw new Exception("Queue is full");
+                Console.WriteLine("Очередь переполнена");
+                return;
+            }
+
+            tail = (tail + 1) % size;
+            data[tail] = item;
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            if (count == 0)
+            {
+                // throw new Exception("Queue is full");
+                Console.WriteLine("Очередь пуста");
+                return default(T);
+            }
+
+            T item = data[head];
+            head = (head + 1) % size;
+            count--;
+            Console.Write("Удален элемент: ");
+            return item;
+        }
+
+        public void Print()
+        {
+            if (count == 0)
+            {
+                // throw new Exception("Queue is full");
+                Console.WriteLine("Очередь пуста");
+                return;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(data[(head + i) % size] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
