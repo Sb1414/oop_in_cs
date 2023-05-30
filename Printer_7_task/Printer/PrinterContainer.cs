ccf48c02f907f9add8eda0f4c9d3bd60f991c02f
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-05-30 09:02:17 +0300

    7 printer

---
+++ Printer_7_task/Printer/PrinterContainer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    class PrinterContainer
+    {
+        private Printer[] printers;
+        private int count;
+
+        public PrinterContainer()
+        {
+            printers = new Printer[4]; // Начальный размер массива
+            count = 0;
+        }
+
+        public int prCn()
+        {
+            return count;
+        }
+
+        public void AddPrinter(Printer printer)
+        {
+            if (count >= printers.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            printers[count] = printer;
+            count++;
+        }
+
+        private void IncreaseCapacity()
+        {
+            int newCapacity = printers.Length * 2; // Увеличение вдвое
+            Printer[] newPrinters = new Printer[newCapacity];
+            Array.Copy(printers, newPrinters, count);
+            printers = newPrinters;
+        }
+
+        public void RemovePrinter(Printer printer)
+        {
+            int index = Array.IndexOf(printers, printer);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine("Принтер не найден в контейнере.");
+            }
+        }
+
+        public void RemovePrinterL(int cartridgeSize)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (printers[i] is LaserPrinter printer && printer.CartridgeSize == cartridgeSize)
+                {
+                    RemoveAt(i);
+                    return;
+                }
+            }
+
+            Console.WriteLine("Принтер с таким размером картриджа не найден.");
+        }
+
+        public void RemovePrinterI(int colorCount)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (printers[i] is InkjetPrinter printer && printer.ColorCount == colorCount)
+                {
+                    RemoveAt(i);
+                    return;
+                }
+            }
+
+            Console.WriteLine("Принтер с таким количеством цветов не найден.");
+        }
+
+        private void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                Console.WriteLine("Некорректный индекс.");
+                return;
+            }
+
+            for (int i = index; i < count - 1; i++)
+            {
+                printers[i] = printers[i + 1];
+            }
+
+            printers[count - 1] = null;
+            count--;
+        }
+
+        public void PrintAll()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (printers[i] != null)
+                {
+                    printers[i].Print();
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public void PrintDetailsAll()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (printers[i] != null)
+                {
+                    printers[i].PrintDetails();
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+}
---
+++ Printer_7_task/Printer/Program.cs
@@ -0,0 +1,127 @@
+﻿using Printer;
+
+bool fl = true;
+int tmpSize = 0;
+PrinterContainer container = new PrinterContainer();
+while (fl)
+{
+    menu();
+    int num;
+    num = Console.ReadKey().KeyChar - '0';
+    switch (num)
+    {
+        case 1:
+            draw();
+            AddLPrinter();
+            break;
+        case 2:
+            draw();
+            AddJPrinter();
+            break;
+        case 3:
+            draw();
+            Console.WriteLine("\nВывод принтеров:");
+            container.PrintDetailsAll();
+            draw();
+            break;
+        case 4:
+            draw();
+            RemoveLaser();
+            draw();
+            break;
+        case 5:
+            draw();
+            RemoveJ();
+            draw();
+            break;
+        default:
+            fl = false;
+            break;
+    }
+}
+
+void AddLPrinter()
+{
+
+    Console.WriteLine("\nСколько принтеров внести?");
+        int n = Console.ReadKey().KeyChar - '0';
+       
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"\nВведите скорость принтера {i}: ");
+        var printSpeed = Console.ReadLine();
+        Console.Write($"\nВведите размер картирижда {i}: ");
+        var cartridgeSize = Console.ReadLine();
+        LaserPrinter laserPrinter = new LaserPrinter(Convert.ToInt32(printSpeed), Convert.ToInt32(cartridgeSize));
+        container.AddPrinter(laserPrinter);
+    }
+
+}
+
+
+void AddJPrinter()
+{
+    Console.WriteLine("\nСколько принтеров внести?");
+
+    int n = Console.ReadKey().KeyChar - '0';
+        
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"\nВведите скорость принтера {i}: ");
+        var printSpeed = Console.ReadLine();
+        Console.Write($"\nВведите количество цветов {i}: ");
+        var colorCount = Console.ReadLine();
+        InkjetPrinter inkjetPrinter = new InkjetPrinter(Convert.ToInt32(printSpeed), Convert.ToInt32(colorCount));
+        container.AddPrinter(inkjetPrinter);
+    }
+
+}
+
+
+void RemoveLaser()
+{
+    if (container.prCn() != 0)
+    {
+        Console.Write("\nВведите данные принтера, который нужно удалить: ");
+        Console.Write($"\nВведите размер картриджа: ");
+        var cartridgeSize = Console.ReadLine();
+        container.RemovePrinterL(Convert.ToInt32(cartridgeSize));
+        tmpSize--;
+    }
+    else
+    {
+        Console.WriteLine("\nНечего удалять");
+    }
+}
+
+void RemoveJ()
+{
+    if (container.prCn() != 0)
+    {
+        Console.Write("\nВведите данные принтера, который нужно удалить: ");
+        Console.Write($"\nВведите количество цветов: ");
+        var colorCount = Console.ReadLine();
+        container.RemovePrinterI(Convert.ToInt32(colorCount));
+        tmpSize--;
+    }
+    else
+    {
+        Console.WriteLine("\nНечего удалять");
+    }
+}
+
+void menu()
+{
+    Console.Write("\n1. Внести данные о лазерном принтере\n");
+    Console.Write("2. Внести данные о струйном принтере\n");
+    Console.Write("3. Вывести все принтеры\n");
+    Console.Write("4. Удалить лазерный принтер\n");
+    Console.Write("5. Удалить струйный принтер\n");
+
+}
+
+
+void draw()
+{
+    Console.WriteLine("\n========================================================");
+}
