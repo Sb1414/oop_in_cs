dea6a35b52ba3b06ecdfa44aa162e9bfb6c4fb5c
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-01 19:07:26 +0300

    Building class

---
+++ oop_construction_firm/WindowsFormsApp1/Building.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Building
+    {
+        public string Address { get; set; }
+        public decimal ConstructionCost { get; set; }
+        public int ConstructionPeriod { get; set; }
+
+        public Building(string address, decimal constructionCost, int constructionPeriod)
+        {
+            Address = address;
+            ConstructionCost = constructionCost;
+            ConstructionPeriod = constructionPeriod;
+        }
+    }
+}
