f1b9da3184bebb7699e32618dc853c44282cc6f9
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-07 17:21:20 +0300

    class Toy

---
+++ ToyStore/ToyStore/Toy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyStore
+{
+    internal class Toy // класс игрушки
+    {
+        public string Name { get; set; } // название
+        public int ArticleNumber { get; set; } // артикул
+        public string Manufacturer { get; set; } // производитель
+        public decimal Price { get; set; } // стоимость
+        public int Quantity { get; set; } // количество
+    }
+}
